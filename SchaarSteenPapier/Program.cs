795912cf6dcc6fc7683b9d5a15ada6cb7248432b
Kenny Passenier <kennypassenier@gmail.com>
2018-02-15 12:04:19 +0100

    Added the players class.

--- SchaarSteenPapier/Program.cs
+++ SchaarSteenPapier/Program.cs
@@ -11,6 +11,15 @@ namespace SchaarSteenPapier
         static void Main(string[] args)
         {
             // This is just to have something in the master branch
+            Speler een = new Speler("Een");
+            Speler twee = new Speler("Twee");
+            for (int teller = 0; teller < 100; teller++)
+            {
+                Console.WriteLine("Een:");
+                Console.WriteLine(een.SpeelBeurt());
+                Console.WriteLine("Twee:");
+                Console.WriteLine(twee.SpeelBeurt());
+            }
         }
     }
 }
---
+++ SchaarSteenPapier/Speler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchaarSteenPapier
+{
+    class Speler
+    {
+        private string naamValue;
+
+        public string Naam
+        {
+            get { return naamValue; }
+            set { naamValue = value; }
+        }
+
+        public Speler(string naam)
+        {
+            this.Naam = naam;
+        }
+
+        public int SpeelBeurt()
+        {
+            return new Random(DateTime.Now.Millisecond).Next(1, 4);
+        }
+
+    }
+}
