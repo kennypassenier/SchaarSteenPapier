77e1833307de279e42e7b6672f59aba2eb05f807
Kenny Passenier <kennypassenier@gmail.com>
2018-02-15 15:30:07 +0100

    Started to work on the class

--- SchaarSteenPapier/Spel.cs
+++ SchaarSteenPapier/Spel.cs
@@ -8,6 +8,27 @@ namespace SchaarSteenPapier
 {
     public class Spel
     {
+        private Speler playerOneValue;
 
+        public Speler PlayerOne
+        {
+            get { return playerOneValue; }
+            set { playerOneValue = value; }
+        }
+
+        private Speler playerTwoValue;
+
+        public Speler PlayerTwo
+        {
+            get { return playerTwoValue; }
+            set { playerTwoValue = value; }
+        }
+
+        public Spel(Speler een, Speler twee)
+        {
+            this.PlayerOne = een;
+            this.PlayerTwo = twee;
+
+        }
     }
 }
